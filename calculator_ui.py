2a7e12a8a1d8928374cf67c3b1bb312f8a950ffc
catdevpete <catdevpete@peter-liang.com>
2025-12-20 12:44:51 +1100

    Finishing up operations work, adding in icon, making buttons themed on grouping

--- calculator_ui.py
+++ calculator_ui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import END, INSERT, Event, StringVar, ttk
 import calculator as calculator
+import calc_icon as ico
+import os
 
 class Calculator_GUI:
     calc = calculator.Calculator()
@@ -9,6 +11,7 @@ class Calculator_GUI:
     target_operation = None
     setting_operation = False
     input_after_op_set = False
+    performed_equal = False
     calc_operation = []
 
     def __init__(self):
@@ -17,37 +20,38 @@ class Calculator_GUI:
         self.target_operation = None
         self.input_after_op_set = None
         self.setting_operation = False
+        self.performed_equal = False
 
         self.calc_operation = [
-        ["%",   lambda: self.set_target_op(self.calc.modulo)],
-        ["CE",  lambda: self.clear_entry],
-        ["C",   lambda: self.all_clear],
-        ["⌫",  lambda: self.calc_input('BackSpace')],
+        ["%",   "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.modulo)],
+        ["CE",  "ButtonOps.TLabel",    lambda: self.clear_entry()],
+        ["C",   "ButtonOps.TLabel",    lambda: self.all_clear()],
+        ["⌫",  "ButtonOps.TLabel",    lambda: self.calc_input('BackSpace')],
 
-        ["¹/𝑥", lambda: None],
-        ["xʸ",  lambda: self.set_target_op(self.calc.exponent)],
-        ["ʸ√𝑥", lambda: self.set_target_op(self.calc.root_extration)],
-        ["÷",   lambda: self.set_target_op(self.calc.division)],
+        ["¹/𝑥", "ButtonOps.TLabel",    lambda: self.perform_one_over_x()],
+        ["𝑥ʸ",  "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.exponent)],
+        ["ʸ√𝑥", "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.root_extration)],
+        ["÷",   "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.division)],
 
-        ["7",   lambda: self.calc_input('7')],
-        ["8",   lambda: self.calc_input('8')],
-        ["9",   lambda: self.calc_input('9')],
-        ["×",   lambda: self.set_target_op(self.calc.multiplication)],
+        ["7",   "ButtonDigits.TLabel", lambda: self.calc_input('7')],
+        ["8",   "ButtonDigits.TLabel", lambda: self.calc_input('8')],
+        ["9",   "ButtonDigits.TLabel", lambda: self.calc_input('9')],
+        ["×",   "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.multiplication)],
   
-        ["4",   lambda: self.calc_input('4')],
-        ["5",   lambda: self.calc_input('5')],
-        ["6",   lambda: self.calc_input('6')],
-        ["-",   lambda: self.set_target_op(self.calc.subtraction)],
+        ["4",   "ButtonDigits.TLabel", lambda: self.calc_input('4')],
+        ["5",   "ButtonDigits.TLabel", lambda: self.calc_input('5')],
+        ["6",   "ButtonDigits.TLabel", lambda: self.calc_input('6')],
+        ["-",   "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.subtraction)],
 
-        ["1",   lambda: self.calc_input('1')],
-        ["2",   lambda: self.calc_input('2')],
-        ["3",   lambda: self.calc_input('3')],
-        ["+",   lambda: self.set_target_op(self.calc.addition)],
+        ["1",   "ButtonDigits.TLabel", lambda: self.calc_input('1')],
+        ["2",   "ButtonDigits.TLabel", lambda: self.calc_input('2')],
+        ["3",   "ButtonDigits.TLabel", lambda: self.calc_input('3')],
+        ["+",   "ButtonOps.TLabel",    lambda: self.set_target_op(self.calc.addition)],
 
-        ["⁺∕₋", lambda: self.toggle_negative],
-        ["0",   lambda: self.calc_input('0')],
-        [".",   lambda: self.calc_input('period')],
-        ["=",   lambda: self.perform_operation()]
+        ["⁺∕₋", "ButtonDigits.TLabel", lambda: self.toggle_negative()],
+        ["0",   "ButtonDigits.TLabel", lambda: self.calc_input('0')],
+        [".",   "ButtonDigits.TLabel", lambda: self.calc_input('period')],
+        ["=",   "ButtonEqual.TLabel",  lambda: self.equal_operation()]
     ]
         
     def set_entry(self, entry):
@@ -68,6 +72,10 @@ class Calculator_GUI:
             self.input_after_op_set = True
 
     def calc_input(self, keysym):
+        if self.performed_equal == True:
+            self.current_value = "0"
+            self.performed_equal = False
+
         num = self.current_value
 
         # Handle input
@@ -121,30 +129,39 @@ class Calculator_GUI:
                 self.current_value = "0"
         else:
             if self.input_after_op_set:
-                self.current_value = self.calc.perform_operation(float(self.current_value.replace(",", "")), self.target_operation)
+                self.current_value = str(self.calc.perform_operation(float(self.current_value.replace(",", "")), self.target_operation))
                 self.set_entry_value(self.current_value)
                 self.setting_operation = True
                 self.input_after_op_set = False
                 self.current_value = "0"
 
+        self.performed_equal = False
         self.target_operation = operation
         return
 
+    def perform_one_over_x(self):
+        self.calc.cache_value = 1.0
+        self.current_value = str(self.calc.perform_operation(float(self.current_value.replace(",", "")), self.calc.division))
+        self.set_entry_value(self.current_value)
+        self.setting_operation = False
+        self.input_after_op_set = False
+        self.performed_equal = True
+    
     def clear_entry(self):
         self.current_value = "0"
-        self.set_entry_value(self.current_value)
+        self.set_entry_value("0")
 
     def all_clear(self):
         self.clear_entry()
         self.calc.all_clear()
 
-    def perform_operation(self):
+    def equal_operation(self):
         if self.setting_operation and self.input_after_op_set:
-            self.current_value = self.calc.perform_operation(float(self.current_value.replace(",", "")), self.target_operation)
+            self.current_value = str(self.calc.perform_operation(float(self.current_value.replace(",", "")), self.target_operation))
             self.set_entry_value(self.current_value)
             self.setting_operation = False
             self.input_after_op_set = False
-            self.current_value = "0"
+            self.performed_equal = True
 
 
 if __name__ == "__main__":
@@ -153,6 +170,9 @@ if __name__ == "__main__":
     root.title("Calculator")
     root.configure(background='#1E1E1E')
     root.geometry('450x600')
+
+    root.iconbitmap(ico.Icon().tempFile)
+    os.remove(ico.Icon().tempFile)
 
     root.rowconfigure(0, weight = 1)
     root.columnconfigure(0, weight = 1)
@@ -166,9 +186,12 @@ if __name__ == "__main__":
     style.configure("TLabel", padding=6,foreground="#D4D4D4", background="#1E1E1E")
     style.configure("BG.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#1E1E1E")
     style.configure("Ent.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#1E1E1E", insertcolor = "#1E1E1E")
-    style.configure("Btn.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#444", anchor = tk.CENTER, font=("Helvetica 14"))
-    style.configure("ActiveBtn.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#444", anchor = tk.CENTER, font=("Helvetica 14"))
-    style.map("Btn.TLabel", background=[('pressed', '!disabled', '#666'), ('active', '#4D4D4D')])
+    style.configure("ButtonDigits.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#555", anchor = tk.CENTER, font=("Helvetica 14"))
+    style.configure("ButtonOps.TLabel", padding=6, relief="flat5", foreground="#D4D4D4", background="#333", anchor = tk.CENTER, font=("Helvetica 14"))
+    style.configure("ButtonEqual.TLabel", padding=6, relief="flat5", foreground="#555", background="#C6EBFA", anchor = tk.CENTER, font=("Helvetica 14"))
+    style.map("ButtonDigits.TLabel", background=[('pressed', '!disabled', '#666'), ('active', '#4D4D4D')])
+    style.map("ButtonOps.TLabel", background=[('pressed', '!disabled', '#666'), ('active', '#4D4D4D')])
+    style.map("ButtonEqual.TLabel", background=[('pressed', '!disabled', '#666'), ('active', "#B3D5E2")])
 
     frame.rowconfigure(0, weight = 1)
     frame.columnconfigure(0, weight = 1)
@@ -202,10 +225,10 @@ if __name__ == "__main__":
             button = ttk.Button(
                 frame,
                 text=calc_ui.calc_operation[col + (row - 1) * 4][0],
-                style="Btn.TLabel"
+                style=calc_ui.calc_operation[col + (row - 1) * 4][1]
             )
             button.grid(row=row, column=col, sticky="nsew", padx=2, pady=2)
-            button.configure(command=calc_ui.calc_operation[col + (row - 1) * 4][1])
+            button.configure(command=calc_ui.calc_operation[col + (row - 1) * 4][2])
             
 
     root.bind("<Key>", calc_ui.process_keypress)
